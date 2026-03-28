26c87727a6a67120c5cbf1e42cbe822f569aa83c
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-03-28 20:04:41 +0530

    update

--- src/main/java/com/smartbiz/config/AppConfig.java
+++ src/main/java/com/smartbiz/config/AppConfig.java
@@ -1,5 +1,4 @@
 package com.smartbiz.config;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
